2b20bf543b898cf9b2cf57202148782e6128e433
Anubhav <choubey.anubhav253@gmail.com>
2026-02-13 22:41:58 +0530

    Updating HELM and frontend for taking ending url dynamically

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -4,7 +4,8 @@
 
 cat > /usr/share/nginx/html/config.js << EOF
 window.ENV = {
-  VITE_API_URL: "${VITE_API_URL}"
+  VITE_API_URL: "${VITE_API_URL}",
+  VITE_STORE_URL_SUFFIX: "${VITE_STORE_URL_SUFFIX}"
 };
 EOF
 
